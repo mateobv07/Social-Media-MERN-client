3e2ae6f6fafec63f2cc22649b68d642336f48110
unknown <bernasconivargas.mateo@gmail.com>
2022-09-02 08:27:07 -0500

    react router

---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Avatar, Button, Paper, Grid, Typography, Container } from "@material-ui/core";
+
+import useStyles from './styles'
+
+const Auth = () => {
+    const classes = useStyles();
+
+    return (
+        <div>
+            AUTH
+        </div>
+    );
+}
+
+export default Auth;

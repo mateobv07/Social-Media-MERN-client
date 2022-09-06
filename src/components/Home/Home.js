7b67e12fb93e38c3963c7073aa196ffc23a7dcf9
unknown <bernasconivargas.mateo@gmail.com>
2022-09-05 19:47:57 -0500

    all

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Container, Grow, Grid } from '@material-ui/core'
+import { Container, Grow, Grid, Paper } from '@material-ui/core'
 import Posts from '../Posts/Posts'
 import Form from '../Form/Form'
 import { useDispatch } from "react-redux";
 import useStyles from './styles'
 
 import { getPosts } from '../../actions/posts'
+import Paginate from "../pagination";
 
 const Home = () => {
     const [currentId, setCurrentId] = useState(null);
@@ -25,6 +26,9 @@ const Home = () => {
                     </Grid>
                     <Grid item xs={12}  md={4}>
                         <Form currentId={currentId}  setCurrentId={setCurrentId}/>
+                        <Paper elevation={6}>
+                            <Paginate />
+                        </Paper>
                     </Grid>
                 </Grid>
             </Container>
